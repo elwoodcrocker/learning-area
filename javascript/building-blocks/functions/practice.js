836d0f348a767448c6714eb407a8cc14969669bc
Elwood Crocker <elwood.crocker@gmail.com>
2023-08-22 16:48:25 -0230

    Practice update and library update

---
+++ javascript/building-blocks/functions/practice.js
@@ -0,0 +1,25 @@
+/*let cars = [];
+    cars[0] = "Saab";
+    cars[1] = "Volvo";
+    cars[2] = "BMW";
+    cars[3] = "Mazda";
+
+
+let bikes = [];
+    bikes[0] = "Harley"
+    bikes[1] = "Susuki"
+    bikes[2] = "Honda"
+    bikes[3] = "Kawazaki"
+
+let trucks = [];
+    trucks[0] = "GMC"
+    trucks[1] = "Dodge"
+    trucks[2] = "Tesla"
+    trucks[3] = "Ford"    
+
+let vehicles = []
+    vehicles[0] = trucks,
+    vehicles[1] = bikes,
+    vehicles[2] = cars;
+
+*/
